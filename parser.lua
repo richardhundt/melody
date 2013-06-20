0684f7657412a303623360695e16b2429db3beb6
Richard Hundt <richardhundt@gmail.com>
2013-06-20 21:41:51 +0200

    yeah, lots

---
+++ parser.lua
@@ -0,0 +1,379 @@
+local util = require("util")
+local re   = require('re')
+local defs = require('parser.defs')
+
+local patt = [[
+   program  <- {|
+      s (<main_stmt> (<sep> s <main_stmt>)* <sep>?)? s (!. / '' => error)
+   |} -> program
+
+   lcomment <- (!%nl %s)* "//" (!%nl .)* %nl
+   bcomment <- "/*" (!"*/" .)* "*/"
+   comment  <- <lcomment> / <bcomment>
+   idsafe   <- !(%alnum / "_")
+   s        <- (<comment> / %s)*
+   S        <- (<comment> / %s)+
+   hs       <- !%nl %s
+   digits   <- %digit (%digit / (&('_' %digit) '_') %digit)*
+   word     <- (%alpha / "_") (%alnum / "_")*
+
+   keyword  <- (
+      "var" / "function" / "class" / "module" / "static"
+      / "new" / "null" / "true" / "false" / "return" / "throw"
+      / "yield" / "await" / "break" / "continue" / "switch"
+      / "case" / "default" / "while" / "do" / "for" / "in" / "of"
+      / "this" / "super" / "import" / "export" / "module"
+      / "try" / "catch" / "finally" / "if" / "else"
+      / "instanceof" / "typeof"
+   ) <idsafe>
+
+   sep     <- <bcomment>? (%nl / ";" / &"}" / <lcomment>) / %s <sep>?
+
+   qstring <- '"' { (!'"' .)* } '"'
+   astring <- "'" { (!"'" .)* } "'"
+
+   special <- "\n" / "\$" / "\\" / "\" .
+
+   rstring <- {|
+      '`' (
+         <raw_expr> / { (<special> / !(<raw_expr> / "`") .)+ }
+      )* '`'
+   |} -> rawString
+
+   raw_expr <- (
+      "${" s <expr> s "}"
+   ) -> rawExpr
+
+   string  <- (
+      <qstring> / <astring>
+   ) -> string
+
+   hexadec <- "-"? "0x" %xdigit+
+
+   decimal <- "-"? <digits> "." <digits> (("e"/"E") "-"? <digits>)? -> tonumber
+
+   integer <- "-"? <digits>
+
+   octal   <- {~ { "-"? "0" [0-7]+ } -> octal ~}
+
+   number  <- {~
+      <hexadec> / <octal> / <decimal> / <integer>
+   ~} -> tonumber
+
+   boolean <- (
+      {"true"/"false"} <idsafe>
+   ) -> boolean
+
+   literal <- ( <number> / <string> / <boolean> ) -> literal
+
+   main_stmt <- (
+        <module_decl>
+      / <import_stmt>
+      / <export_decl>
+      / <stmt>
+   )
+
+   module_decl <- (
+      "module" <idsafe> s <ident> s "{" s
+         {| (<main_stmt> (<sep> s <main_stmt>)*)? |} s
+      "}"
+   ) -> moduleDecl
+
+   export_decl <- (
+      "export" <idsafe> s (<decl_stmt> / <module_decl>)
+   ) -> exportDecl
+
+   import_stmt <- (
+      "import" <idsafe> s {| {"*"} / "{" s <ident> (s "," s <ident>)* s "}" |} s
+      "from" s <string>
+   ) -> importStmt
+
+   stmt <- (
+      <if_stmt>
+      / <while_stmt>
+      / <for_stmt>
+      / <for_of_stmt>
+      / <expr_stmt>
+      / <decl_stmt>
+      / <return_stmt>
+      / <break_stmt>
+      / <throw_stmt>
+      / <try_stmt>
+      / <switch_stmt>
+   )
+
+   stmt_list <- {|
+      (<stmt> (<sep> s <stmt>)* <sep>?)?
+   |}
+
+   switch_stmt <- (
+      "switch" <idsafe> s "(" s <expr> s ")" s "{" s
+         {| <switch_case>* |}
+         (s "default" <idsafe> s ":" s (<block_stmt> / <stmt_list>))? s
+      "}"
+   ) -> switchStmt
+
+   switch_case <- (
+      s "case" <idsafe> s <expr> s ":" s (<block_stmt> / <stmt_list>)
+   ) -> switchCase
+
+   break_stmt <- (
+      "break" <idsafe>
+   ) -> breakStmt
+
+   throw_stmt <- (
+      "throw" <idsafe> s <expr>
+   ) -> throwStmt
+
+   try_stmt <- (
+      "try" <idsafe> s <block_stmt>
+      {| <catch_clause>* |} (s "finally" <idsafe> s <block_stmt>)?
+   ) -> tryStmt
+
+   catch_clause <- (
+      s "catch" <idsafe> s "(" s
+      <ident> (s "if" <idsafe> s <expr>)? s ")" s <block_stmt> s
+   ) -> catchClause
+
+   return_stmt <- (
+      "return" <idsafe> s <expr>
+   ) -> returnStmt
+
+   decl_stmt <- (
+      <var_decl> / <func_decl> / <class_decl>
+   )
+
+   var_decl <- (
+      "var" <idsafe> s <patt> (s "=" s <expr>)?
+   ) -> varDecl
+
+   patt <- (
+      <array_patt> / <object_patt> / <member_expr>
+   )
+
+   array_patt <- (
+      "[" s {| <patt> (s "," s <patt>)* |} "]"
+   ) -> arrayPatt
+
+   object_patt <- (
+      "{" s {| <object_patt_pair> (s "," s <object_patt_pair>)* |} "}"
+   ) -> objectPatt
+   object_patt_pair <- (
+      (<literal> / <ident>) s ":" s <patt>
+   )
+
+   func_decl <- (
+      "function" <idsafe> s <ident> "(" s {| <param_list>? |} s ")" s <func_body>
+   ) -> funcDecl
+
+   func_expr <- (
+      "function" <idsafe> s "(" s {| <param_list>? |} s ")" s <func_body>
+      / "(" s {| <param_list>? |} s ")" s "=>" s <func_body>
+      / {| {| {:name: <ident> :} |} |} s "=>" s <func_body>
+   ) -> funcExpr
+
+   func_body <- <block_stmt> / <expr>
+
+   class_decl <- (
+      "class" <idsafe> s <ident> (s <class_heritage>)? s "{" s <class_body> s "}"
+   ) -> classDecl
+
+   class_body <- {|
+      (<class_member> (<sep> s <class_member>)* <sep>?)?
+   |}
+
+   class_member <- (
+      ({"static"} <idsafe> s / '' -> "virtual") <prop_defn>
+   ) -> classMember
+
+   class_heritage <- (
+      "extends" <idsafe> s <expr> / {| |}
+   )
+
+   prop_defn <- (
+      ({"get"/"set"} s / '' -> "init") <ident> s
+      "(" s {| <param_list>? |} s ")" s
+      <func_body>
+   ) -> propDefn
+
+   param <- {|
+      {:name: <ident> :} (s "=" s {:default: <expr> :})?
+   |}
+   param_list <- (
+        <param> s "," s <param_list>
+      / <param> s "," s <param_rest>
+      / <param>
+      / <param_rest>
+   )
+
+   param_rest <- {| "..." {:name: <ident> :} {:rest: '' -> 'true' :} |}
+
+   block_stmt <- (
+      "{" s {| (<stmt> (<sep> s <stmt>)* <sep>?)? |} s "}"
+   ) -> blockStmt
+
+   if_stmt <- (
+      "if" <idsafe> s "(" s <expr> s ")" s <if_body>
+      (s "else" <idsafe> s <if_stmt> / s "else" <idsafe> s <if_body>)?
+   ) -> ifStmt
+
+   if_body <- <block_stmt> / <expr_stmt>
+
+   for_stmt <- (
+      "for" <idsafe> s "(" s
+      {| <for_init>?  |} s ";" s
+      {| <expr>?      |} s ";" s
+      {| <expr_stmt>? |} s ")" s
+      <loop_body>
+   ) -> forStmt
+   for_init <- <var_decl> / <expr_stmt>
+
+   for_of_stmt <- (
+      "for" <idsafe> s "(" s <for_of_init> s "of" <idsafe> s <expr> s ")" s
+      <loop_body>
+   ) -> forOfStmt
+   for_of_init <- <var_decl> / <patt>
+
+   loop_body <- <block_stmt> / <expr_stmt>
+
+   while_stmt <- (
+      "while" <idsafe> s "(" s <expr> s ")" s <loop_body>
+   ) -> whileStmt
+
+   ident <- (
+      !<keyword> { (%alpha / "_") (%alnum / "_")* }
+   ) -> identifier
+
+   term <- (
+        <func_expr>
+      / <null_expr>
+      / <this_expr>
+      / <super_expr>
+      / <comp_expr>
+      / <array_expr>
+      / <object_expr>
+      / <regex_expr>
+      / <ident>
+      / <literal>
+      / <rstring>
+      / "(" s <expr> s ")"
+   )
+
+   expr <- <infix_expr> / <new_expr> / <spread_expr>
+
+   spread_expr <- (
+      "..." <postfix_expr>
+   ) -> spreadExpr
+
+   this_expr <- (
+      "this" <idsafe>
+   ) -> thisExpr
+
+   null_expr <- (
+      "null" <idsafe>
+   ) -> nullExpr
+
+   super_expr <- (
+      "super" <idsafe>
+   ) -> superExpr
+
+   expr_stmt <- (
+      {} (<assign_expr> / !("{" / ("class" / "function")<idsafe>) <expr>)
+   ) -> exprStmt
+
+   binop <- {
+      "+" / "-" / "~" / "/" / "*" / "%" / "^" / "||" / "&&"
+      / "|" / "&" / ">>>" / ">>" / ">=" / ">" / "<<" / "<=" / "<"
+      / "!==" / "===" / "!=" / "==" / "instanceof" <idsafe>
+   }
+
+   infix_expr  <- (
+      {| <prefix_expr> (s <binop> s <prefix_expr>)+ |}
+   ) -> infixExpr / <prefix_expr>
+
+   prefix_expr <- (
+      { "++" / "--" / "~" / "+" / "-" / "!" / "typeof" <idsafe> } s <prefix_expr>
+   ) -> prefixExpr / <postfix_expr>
+
+   postfix_expr <- {|
+      <term> (s <postfix_tail>)+
+   |} -> postfixExpr / <term>
+
+   postfix_tail <- {|
+        { "." } s <ident>
+      / { "[" } s <expr> s ("]" / '' => error)
+      / { "(" } s {| (<expr> (s "," s <expr>)*)? |} s (")" / '' => error)
+      / { "++" / "--" }
+   |}
+
+   update_expr <- (
+      <member_expr> {"++" / "--"}
+   ) -> updateExpr
+
+   member_expr <- {|
+      <term> (s <member_next>)?
+   |} -> postfixExpr / <term>
+   member_next <- (
+      <postfix_tail> s <member_next> / <member_tail>
+   )
+   member_tail <- {|
+        { "." } s <ident>
+      / { "[" } s <expr> s ("]" / '' => error)
+   |}
+
+   assop <- {
+      "=" / "+=" / "-=" / "~=" / "*=" / "/=" / "%="
+      / "|=" / "&=" / "^=" / "<<=" / ">>>=" / ">>="
+   }
+
+   assign_expr <- (
+      <patt> s <assop> s <expr>
+   ) -> assignExpr
+
+   new_expr <- (
+      "new" <idsafe> s <member_expr> (
+         s "(" s {| (<expr> (s "," s <expr>)*)? |} s ")"
+         / {| |}
+      )
+   ) -> newExpr
+
+   array_expr <- (
+      "[" s {| <array_elements>? |} s "]"
+   ) -> arrayExpr
+
+   array_elements <- <expr> (s "," s <expr>)* (s ",")?
+
+   object_expr <- (
+      "{" s {| <object_members>? |} s "}"
+   ) -> objectExpr
+
+   object_members <- (
+      <object_member> (s "," s <object_member>)* (s ",")?
+   )
+   object_member <- (<prop_defn> / {|
+      {:key: (<literal> / <ident>) :} s ":" s {:value: <expr> :}
+   |}) -> objectMember
+
+   comp_expr <- (
+      "[" s {| <comp_block>+ |} <expr> (s "if" <idsafe> s "(" s <expr> s ")")? s "]"
+   ) -> compExpr
+
+   comp_block <- (
+      "for" <idsafe> s "(" s <patt> s "of" <idsafe> s <expr> s ")" s
+   ) -> compBlock
+
+   regex_expr <- (
+      "/" { ( "\\" / "\/" / !("/" / %nl) .)* } "/" {[gmi]*}
+   ) -> regexExpr
+]]
+
+local grammar = re.compile(patt, defs)
+local function parse(src)
+   return grammar:match(src)
+end
+
+return {
+   parse = parse
+}
+
+
---
+++ transformer.lua
@@ -0,0 +1,626 @@
+local B = require('builder')
+local util = require('util')
+
+local match = { }
+
+function match:Program(node)
+   self.hoist = { }
+   self.scope = { }
+   local export = B.identifier('export')
+   for i=1, #node.body do
+      local stmt = self:get(node.body[i])
+      self.scope[#self.scope + 1] = stmt
+   end
+   for i=#self.hoist, 1, -1 do
+      table.insert(self.scope, 1, self.hoist[i])
+   end
+   return B.chunk(self.scope)
+end
+function match:ModuleDeclaration(node)
+   local name = self:get(node.id)
+   self.hoist[#self.hoist + 1] = B.localDeclaration({ name }, { })
+
+   local outer_scope = self.scope
+   local outer_hoist = self.hoist
+
+   self.scope = { }
+   self.hoist = { }
+
+   local export = B.identifier('export')
+   self.scope[#self.scope + 1] = B.localDeclaration({ export }, { B.table({}) })
+
+   for i=1, #node.body do
+      local stmt = self:get(node.body[i])
+      self.scope[#self.scope + 1] = stmt
+   end
+   for i=#self.hoist, 1, -1 do
+      table.insert(self.scope, 1, self.hoist[i])
+   end
+
+   local body = self.scope
+
+   self.scope = outer_scope
+   self.hoist = outer_hoist
+
+   body[#body + 1] = B.returnStatement({ export })
+
+   return B.assignmentExpression({ name }, {
+      B.callExpression(
+         B.parenExpression{
+            B.functionExpression({ }, B.blockStatement(body))
+         }, { }
+      )
+   })
+end
+function match:Literal(node)
+   return B.literal(node.value)
+end
+function match:Identifier(node)
+   return B.identifier(node.name)
+end
+function match:VariableDeclaration(node)
+   local init = node.init and self:get(node.init) or nil
+   if node.export then
+      local expr = B.memberExpression(B.identifier('export'), self:get(node.id))
+      self.scope[#self.scope + 1] = B.assignmentExpression(
+         { expr }, { init }
+      )
+      init = expr
+   end
+   return B.localDeclaration({ self:get(node.id) }, { init })
+end
+function match:AssignmentExpression(node)
+   if #node.operator == 2 then
+      local oper = string.sub(node.operator, 1, 1)
+      return B.assignmentExpression({
+         self:get(node.left)
+      }, {
+         B.binaryExpression(oper, self:get(node.left), self:get(node.right))
+      })
+   end
+   return B.assignmentExpression({ self:get(node.left) }, { self:get(node.right) })
+end
+function match:MemberExpression(node)
+   return B.memberExpression(
+      self:get(node.object), self:get(node.property), node.computed
+   )
+end
+function match:ThisExpression(node)
+   return B.identifier('this')
+end
+function match:SuperExpression(node)
+   return B.identifier('super')
+end
+function match:ReturnStatement(node)
+   if self.retsig then
+      return B.doStatement(
+         B.blockStatement{
+            B.assignmentExpression(
+               { self.retsig }, { B.literal(true) }
+            );
+            B.assignmentExpression(
+               { self.retval }, { self:get(node.argument) }
+            );
+            B.returnStatement{ self.retval }
+         }
+      )
+   end
+   return B.returnStatement{ self:get(node.argument) }
+end
+function match:IfStatement(node)
+   local test, cons, altn = self:get(node.test)
+   if node.consequent then
+      cons = self:get(node.consequent)
+   end
+   if node.alternate then
+      altn = self:get(node.alternate)
+   end
+   local stmt = B.ifStatement(test, cons, altn)
+   return stmt
+end
+
+function match:TryStatement(node)
+   local oldret = self.retsig
+   local oldval = self.retval
+
+   self.retsig = B.tempnam()
+   self.retval = B.tempnam()
+
+   local try = B.functionExpression({ }, self:get(node.body))
+
+   local finally
+   if node.finalizer then
+      finally = B.functionExpression({ }, self:get(node.finalizer))
+   end
+
+   local clauses = { }
+   for i=#node.guardedHandlers, 1, -1 do
+      local clause = node.guardedHandlers[i]
+      local cons = self:get(clause.body)
+      local head = B.localDeclaration(
+         { self:get(clause.param) }, { B.vararg() }
+      )
+      cons.body[#cons.body + 1] = B.breakStatement()
+      clauses[#clauses + 1] = head
+      clauses[#clauses + 1] = B.ifStatement(self:get(clause.guard), cons)
+   end
+   if node.handler then
+      local clause = node.handler
+      local cons = self:get(clause.body)
+      local head = B.localDeclaration(
+         { self:get(clause.param) }, { B.vararg() }
+      )
+      cons.body[#cons.body + 1] = B.breakStatement()
+      clauses[#clauses + 1] = head
+      clauses[#clauses + 1] = B.doStatement(cons)
+   end
+
+   local catch = B.functionExpression(
+      { B.vararg() }, B.blockStatement{
+         B.repeatStatement(B.literal(true), B.blockStatement(clauses))
+      }
+   )
+
+   local expr = B.callExpression(B.identifier('try'), { try, catch, finally })
+   local temp = self.retval
+   local rets = self.retsig
+
+   self.retsig = oldret
+   self.retval = oldval
+
+   return B.doStatement(
+      B.blockStatement{
+         B.localDeclaration({ rets }, { B.literal(false) });
+         B.localDeclaration({ temp }, { B.literal(nil) });
+         B.expressionStatement(expr);
+         B.ifStatement(
+            rets, B.blockStatement{ B.returnStatement{ temp } }
+         )
+      }
+   )
+end
+function match:SwitchStatement(node)
+   local body = { }
+   local disc = B.tempnam()
+
+   local cases = { }
+   for i=1, #node.cases do
+      local case = node.cases[i]
+      local test
+      if case.test then
+         test = B.binaryExpression("==", disc, self:get(case.test))
+      else
+         test = B.literal(true)
+      end
+      cases[#cases + 1] = B.ifStatement(
+         test, B.blockStatement(self:list(case.consequent))
+      )
+   end
+   body[#body + 1] = B.localDeclaration({ disc }, { self:get(node.discriminant) })
+   body[#body + 1] = B.repeatStatement(
+      B.literal(true), B.blockStatement(cases)
+   )
+   return B.doStatement(B.blockStatement(body))
+end
+function match:BreakStatement(node)
+   return B.breakStatement()
+end
+function match:ThrowStatement(node)
+   return B.expressionStatement(
+      B.callExpression(B.identifier('throw'), { self:get(node.argument) }) 
+   )
+end
+
+function match:LogicalExpression(node)
+   local o = node.operator
+   if o == '&&' then
+      o = 'and'
+   elseif o == '||' then
+      o = 'or'
+   end
+   return B.logicalExpression(o, self:get(node.left), self:get(node.right))
+end
+
+local bitop = {
+   [">>"]  = 'rshift',
+   [">>>"] = 'arshift',
+   ["<<"]  = 'lshift',
+   ["|"]   = 'bor',
+   ["&"]   = 'band',
+   ["^"]   = 'bxor',
+}
+function match:BinaryExpression(node)
+   local o = node.operator
+   if bitop[o] then
+      local call = B.memberExpression(
+         B.identifier('bit'),
+         B.identifier(bitop[o])
+      )
+      local args = { self:get(node.left), self:get(node.right) }
+      return B.callExpression(call, args)
+   elseif o == 'instanceof' then
+      return B.callExpression(
+         B.identifier('instanceof'), { self:get(node.left), self:get(node.right) }
+      )
+   end
+   if o == '~' then o = '..' end
+   return B.binaryExpression(o, self:get(node.left), self:get(node.right))
+end
+function match:FunctionDeclaration(node)
+   local name
+   if not node.expression then
+      name = self:get(node.id)
+   end
+
+   local params  = { }
+   local prelude = { }
+   local vararg  = false
+
+   prelude[#prelude + 1] = B.localDeclaration(
+      { B.identifier('this') }, {
+      B.logicalExpression("or", B.identifier('self'), B.identifier('this'))
+   })
+
+   for i=1, #node.params do
+      params[#params + 1] = self:get(node.params[i])
+      if node.defaults[i] then
+         local name = self:get(node.params[i])
+         local test = B.binaryExpression("==", name, B.literal(nil))
+         local expr = self:get(node.defaults[i])
+         local cons = B.blockStatement{
+            B.assignmentExpression({ name }, { expr })
+         }
+         prelude[#prelude + 1] = B.ifStatement(test, cons)
+      end
+   end
+
+   if node.rest then
+      params[#params + 1] = B.vararg()
+      prelude[#prelude + 1] = B.localDeclaration(
+         { B.identifier(node.rest.name) },
+         { B.callExpression(
+            B.identifier('new'),
+            { B.identifier('Array'), B.vararg() }
+         ) }
+      )
+   end
+
+   local body = self:get(node.body)
+   for i=#prelude, 1, -1 do
+      table.insert(body.body, 1, prelude[i])
+   end
+
+   if node.expression then
+      return B.functionExpression(params, body, vararg)
+   else
+      return B.functionDeclaration(name, params, body, vararg)
+   end
+end
+--[[
+   local Point = class("Point", function(this, super)
+      Object:defineProperties(this, {
+         move = {
+            value = function(self, x, y)
+
+            end,
+         }
+      })
+   end)
+]]
+function match:ClassDeclaration(node)
+   local name = self:get(node.id)
+   local base = node.base and self:get(node.base) or B.identifier('Object')
+
+   local properties = { }
+
+   for i=1, #node.body do
+      local prop = node.body[i]
+      local desc = properties[prop.key.name] or { }
+      if prop.kind == 'get' then
+         desc.get = self:get(prop)
+      elseif prop.kind == 'set' then
+         desc.set = self:get(prop)
+      else
+         desc.value = self:get(prop)
+      end
+      if desc.static then
+         if desc.static.value ~= prop.static then
+            error("property "..prop.key.name.." already defined as static")
+         end
+      end
+      desc.static = B.literal(prop.static)
+      properties[prop.key.name] = desc
+   end
+
+   for k,v in pairs(properties) do
+      properties[k] = B.table(v)
+   end
+
+   local body = {
+      B.expressionStatement(B.sendExpression(
+            B.identifier("Object"),
+            B.identifier("defineProperties"),
+            { B.identifier("this"), B.table(properties) }
+         )
+      )
+   }
+   self.hoist[#self.hoist + 1] = B.localDeclaration({ name }, { })
+
+   local init = B.callExpression(
+      B.identifier('class'), {
+         B.literal(node.id.name), base,
+         B.functionExpression(
+            { B.identifier('this'), B.identifier('super') },
+            B.blockStatement(body)
+         )
+      }
+   )
+
+   if node.export then
+      local expr = B.memberExpression(B.identifier('export'), name)
+      self.scope[#self.scope + 1] = B.assignmentExpression(
+         { expr }, { init }
+      )
+      init = expr
+   end
+
+   return B.assignmentExpression(
+      { name }, { init }
+   )
+end
+function match:SpreadExpression(node)
+   return B.callExpression(
+      B.identifier('__spread__'), { self:get(node.argument) }
+   )
+end
+function match:NullExpression(node)
+   return B.identifier('null')
+end
+function match:PropertyDefinition(node)
+   return self:get(node.value)
+end
+function match:BlockStatement(node)
+   return B.blockStatement(self:list(node.body))
+end
+function match:ExpressionStatement(node)
+   return B.expressionStatement(self:get(node.expression))
+end
+function match:CallExpression(node)
+   local callee = node.callee
+   if callee.type == 'MemberExpression' and not callee.computed then
+      if callee.object.type == 'SuperExpression' then
+         local args = self:list(node.arguments)
+         local recv = B.memberExpression(
+            B.memberExpression(
+               B.identifier('super'), B.identifier('__methods__')
+            ),
+            self:get(callee.property)
+         )
+         table.insert(args, 1, B.identifier('this'))
+         return B.callExpression(recv, args)
+      else
+         local recv = self:get(callee.object)
+         local prop = self:get(callee.property)
+         return B.sendExpression(recv, prop, self:list(node.arguments))
+      end
+   else
+      if callee.type == 'SuperExpression' then
+         local args = self:list(node.arguments)
+         local recv = B.memberExpression(
+            B.memberExpression(
+               B.identifier('super'), B.identifier('__methods__')
+            ),
+            B.identifier('constructor')
+         )
+         table.insert(args, 1, B.identifier('this'))
+         return B.callExpression(recv, args)
+      else
+         local args = self:list(node.arguments)
+         table.insert(args, 1, B.literal(nil))
+         return B.callExpression(self:get(callee), args)
+      end
+   end
+end
+function match:NewExpression(node)
+   return B.callExpression(B.identifier('new'), {
+      self:get(node.callee), unpack(self:list(node.arguments))
+   })
+end
+function match:ForStatement(node)
+   local body = self:get(node.body)
+   if node.post[1] then
+      body.body[#body.body + 1] = self:get(node.post[1]);
+   end
+
+   local init
+   if node.init[1] then
+      init = self:get(node.init[1])
+   else
+      init = B.emptyStatement()
+   end
+
+   local test
+   if node.test[1] then
+      test = self:get(node.test[1])
+   else
+      test = B.literal(true)
+   end
+
+   return B.doStatement(
+      B.blockStatement{ init, B.whileStatement(test, body) }
+   )
+end
+function match:ArrayExpression(node)
+   local args = { B.identifier('Array') }
+   for i=1, #node.elements do
+      args[#args + 1] = self:get(node.elements[i])
+   end
+   return B.callExpression(B.identifier('new'), args)
+end
+function match:ObjectExpression(node)
+   local properties = { }
+   for i=1, #node.properties do
+      local prop = node.properties[i]
+
+      local key, val
+      if prop.key.type == 'Identifier' then
+         key = prop.key.name
+      elseif prop.key.type == "Literal" then
+         key = prop.key.value
+      end
+
+      local desc = properties[key] or { }
+
+      if prop.kind == 'get' then
+         desc.get = self:get(prop.value)
+      elseif prop.kind == 'set' then
+         desc.set = self:get(prop.value)
+      else
+         desc.value = self:get(prop.value)
+      end
+
+      properties[key] = desc
+   end
+
+   for k,v in pairs(properties) do
+      properties[k] = B.table(v)
+   end
+
+   return B.sendExpression(
+      B.identifier('Object'), B.identifier("create"), {
+         B.literal(nil);
+         B.table(properties);
+      }
+   )
+end
+function match:ForOfStatement(node)
+   local none = B.tempnam()
+   local temp = B.tempnam()
+   local iter = B.callExpression(B.identifier('pairs'), { self:get(node.right) })
+   local left = self:get(node.left)
+   local body = self:get(node.body)
+   if node.left.type == 'VariableDeclaration' then
+      left = self:get(node.left.id)
+   else
+      table.insert(body.body, 1, B.assignmentExpression({ left }, { temp }))
+      left = temp
+   end
+   return B.forInStatement(B.forNames{ none, left }, iter, body)
+end
+function match:RegExp(node)
+   return B.callExpression(
+      B.identifier('new'), {
+         B.identifier('RegExp'),
+         B.literal(node.pattern),
+         B.literal(node.flags)
+      }
+   )
+end
+function match:RawString(node)
+   local list = { }
+   local tostring = B.identifier('tostring')
+   for i=1, #node.expressions do
+      local expr = node.expressions[i]
+      if type(expr) == 'string' then
+         list[#list + 1] = B.literal(expr)
+      else
+         list[#list + 1] = B.callExpression(tostring, { self:get(expr.expression) })
+      end
+   end
+   return B.listExpression('..', list)
+end
+function match:ArrayComprehension(node)
+   local temp = B.tempnam()
+   local body = B.blockStatement{
+      B.localDeclaration({ temp }, {
+         B.callExpression(
+            B.identifier('new'), {  B.identifier('Array') }
+         )
+      })
+   }
+   local last = body
+   for i=1, #node.blocks do
+      local loop = self:get(node.blocks[i])
+      last.body[#last.body + 1] = loop
+      last = loop.body
+   end
+   last.body[#last.body + 1] = B.assignmentExpression({
+      B.memberExpression(temp, B.unaryExpression('#', temp), true)
+   }, {
+      self:get(node.body)    
+   })
+   body.body[#body.body + 1] = B.returnStatement{ temp }
+   return B.callExpression(
+      B.parenExpression{
+         B.functionExpression({ }, body)
+      }, { }
+   )
+end
+function match:ComprehensionBlock(node)
+   local none = B.tempnam()
+   local temp = B.tempnam()
+   local iter = B.callExpression(B.identifier('pairs'), { self:get(node.right) })
+   local left = self:get(node.left)
+   local body = { }
+   if node.left.type == 'Identifier' then
+      left = self:get(node.left)
+   else
+      table.insert(body, 1, B.assignmentExpression({ left }, { temp }))
+      left = temp
+   end
+   return B.forInStatement(B.forNames{ none, left }, iter, B.blockStatement(body))
+end
+
+local function countln(src, pos, idx)
+   local line = 0
+   local index, limit = idx or 1, pos
+   while index <= limit do
+      local s, e = string.find(src, "\n", index, true)
+      if s == nil or e > limit then
+         break
+      end
+      index = e + 1
+      line  = line + 1
+   end
+   return line 
+end
+
+local function transform(tree, src)
+   local self = { }
+   self.line = 0
+   self.pos  = 0
+
+   function self:sync(node)
+      local pos = node.pos
+      if pos ~= nil and pos > self.pos then
+         local prev = self.pos
+         local line = countln(src, pos, prev + 1) + self.line
+         self.line = line
+         self.pos = pos
+      end
+   end
+
+   function self:get(node)
+      if not match[node.type] then
+         error("no handler for "..tostring(node.type))
+      end
+      self:sync(node)
+      local out = match[node.type](self, node)
+      out.line = self.line
+      return out
+   end
+
+   function self:list(nodes)
+      local list = { }
+      for i=1, #nodes do
+         list[#list + 1] = self:get(nodes[i])
+      end
+      return list
+   end
+
+   return self:get(tree)
+end
+
+return {
+   transform = transform
+}
